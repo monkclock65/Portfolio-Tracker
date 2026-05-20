621bdf72b5bef1ffa8b83a0ec54a66f23fd803ec
zack <zackinceok17@gmail.com>
2026-05-20 19:14:54 +0000

    create model init and user model

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.portfolio import Portfolio
+from app.models.transaction import Transaction
+from app.models.pricecache import PriceCache
+from app.models.watchlist import Watchlist
---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    hashed_passwd = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    portfolio = db.relationship('Portfolio', backref='user', lazy=True)
+    watchlist = db.relationship('Watchlist', backref='user', lazy=True)
