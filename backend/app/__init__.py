acd90b6ef4c7d944bfbd63b6ad3fd045498146d6
zack <zackinceok17@gmail.com>
2026-05-24 18:52:52 +0000

    add bcrypt package for auth

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from app.config import Config
-from app.extensions import db, migrate, jwt
+from app.extensions import db, migrate, jwt, bcrypt
 from flask_migrate import Migrate
 
 def create_app(test_config=None):
@@ -15,6 +15,7 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+    bcrypt.init_app(app)
     with app.app_context():
         from app import routes, models
         
--- backend/app/models/watchlist.py
+++ backend/app/models/watchlist.py
@@ -2,6 +2,7 @@ from app.extensions import db
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 
+
 class Watchlist(db.Model):
     __tablename__ = 'watchlist'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
