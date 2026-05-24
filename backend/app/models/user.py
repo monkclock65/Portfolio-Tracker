903f1bc646ad464f3b3c084138d497b93778e4ce
zack <zackinceok17@gmail.com>
2026-05-24 19:29:47 +0000

    fix datetime error in user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,7 @@
 from app.extensions import db
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
-import datetime
+from datetime import datetime, timezone
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -9,7 +9,7 @@ class User(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     hashed_passwd = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime)
-    
-    portfolio = db.relationship('Portfolio', backref='user', lazy=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    portfolios = db.relationship('Portfolio', backref='user', lazy=True)
     watchlist = db.relationship('Watchlist', backref='user', lazy=True)
