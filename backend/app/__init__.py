3b179d812c79bf36fe38f91ad7cb35732817d5b7
zack <zackinceok17@gmail.com>
2026-08-30 18:38:07 -0500

    fix cors blocking

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -31,6 +31,7 @@ def create_app(test_config=None):
                 "http://127.0.0.1:5173",
                 "http://localhost:5174",
                 "http://127.0.0.1:5174",
+                "https://portfolio-tracker-actk.onrender.com"
             ],
             "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"],
--- frontend/frontend-app/src/api.js
+++ frontend/frontend-app/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://portfolio-tracker-actk.onrender.com/',
+  baseURL: 'https://portfolio-tracker-actk.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
